61f7466a95937633348bc7b31e8bd9dbc20c0b18
yoo-jimin127 <dbwlals9936@naver.com>
2021-06-20 21:37:29 +0900

    migration 추가하기

--- webstagram/webstagramapp/views.py
+++ webstagram/webstagramapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from django.contrib.auth.models import User
+from .models import Content
 
 # Create your views here.
+
+def feed(request, id):
+    contents=get_object_or_404(Content,pk=id)
+    return render(request, 'feed.html', {'post':contents})
+
